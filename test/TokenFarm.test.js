5d366f9472efd638b2b96994b1280f47d465d4bc
pranavvdesai <pranavdesai.psd@gmail.com>
2021-10-14 22:32:55 +0530

    init tests

---
+++ test/TokenFarm.test.js
@@ -0,0 +1,8 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm")
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
