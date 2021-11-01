1b6421708fb0f6ecd2b2f0d7524557766d5f3467
pranavvdesai <pranavdesai.psd@gmail.com>
2021-11-02 02:14:48 +0530

    deposit DAIs

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,9 +9,29 @@ contract TokenFarm{
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    mapping(address => uint) public stakingBalance
+
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     
     }
+
+    // Deposit
+    function stake(uint _amount) public payable{
+
+        // transfer from investor to this contract
+        daiToken.transferFrom(msg.sender,address(this), _amount);
+    
+        // add to staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+    
+    }
+    // withdraw
+    function unstake(uint _amount) public{
+        dappToken.transfer(msg.sender, _amount);
+    }
+
+    // issue tokens
 }
